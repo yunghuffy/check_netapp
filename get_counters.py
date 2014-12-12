02d1ca8267706ce8d48d7ef6172953e203f475b7
champain <jacobcastello@gmail.com>
2014-12-12 16:52:02 -0500

    Trying to shift to object-orientation

--- get_counters.py
+++ get_counters.py
@@ -1,57 +1,59 @@
 from NetApp.NaServer import *
 
+class GetCounters():
+    def __init__(self, na_object):
+	self.na_object = na_object
+        # Start out by building the xml necessary for the request
+        self.get_counters = NaElement("perf-object-counter-list-info")
+        self.get_counters.child_add_string("objectname", self.na_object)
+        self.get_counters.sprintf()
+        # Invoke the server
+        self.counters_out = server.invoke_elem(self.get_counters)
+        # Lot of shit to walk down here
+        self.counters_list = self.counters_out.children_get()
+
+    def getOut(self):
+	self.count_dict = {}
+        for self.i in self.counters_list:
+            self.counter_info = self.i.children_get()
+            for self.count in self.counter_info:
+       	        self.count_desc = self.count.child_get_string("desc")
+       	        self.count_name = self.count.child_get_string("name")
+       	        self.count_unit = self.count.child_get_string("unit")
+   		self.count_list = [self.count_desc, self.count_unit]
+       	        self.count_dict[self.count_name] = self.count_list
+	    return self.count_dict
+
 # Let's gather all the counters for perf_objects
-def get_counters(na_object):
-
-    # Start out by building the xml necessary for the request
-    get_counters = NaElement("perf-object-counter-list-info")
-    get_counters.child_add_string("objectname", na_object)
-    get_counters.sprintf()
-    # Invoke the server
-    counters_out = server.invoke_elem(get_counters)
+class GetAllObj():
+    def __init(self):
+	return None
+   	# Function to gather all performance objects into one array	
+    def getList(self):
+        # Very simple API call here
+        self.get_obj = NaElement("perf-object-list-info")
     
-
-    # Lot of shit to walk down here
-    counters_list = counters_out.children_get()
-    count_dict = {}
-    for i in counters_list:
-	counter_info = i.children_get()
-	for count in counter_info:
-        	count_desc = count.child_get_string("desc")
-        	count_name = count.child_get_string("name")
-        	count_unit = count.child_get_string("unit")
-    		count_list = [count_desc, count_unit]
-		count_dict[count_name] = count_list
-	return count_dict
-
-
-# Function to gather all performance objects into one array	
-def get_all_obj():	
+        # Invoke the server
+        self.obj_out = server.invoke_elem(self.get_obj)
+        self.obj_out.sprintf()
     
-    # Very simple API call here
-    get_obj = NaElement("perf-object-list-info")
-    
-    # Invoke the server
-    obj_out = server.invoke_elem(get_obj)
-    obj_out.sprintf()
-    
-    # Walk down the xml
-    obj_list = obj_out.children_get()
-    obj_name_list = []
-    for i in obj_list:
-        obj_info = i.children_get()
-        for obj in obj_info:
-            obj_name = obj.child_get_string("name")
-            obj_name_list.append(obj_name)
-    return obj_name_list
+        # Walk down the xml
+        self.obj_list = self.obj_out.children_get()
+        self.obj_name_list = []
+	for self.i in self.obj_list:
+            self.obj_info = self.i.children_get()
+            for self.obj in self.obj_info:
+                self.obj_name = self.obj.child_get_string("name")
+                self.obj_name_list.append(self.obj_name)
+        return self.obj_name_list
 
 
-# Replace this with your info
-server = NaServer(host, 1, 21)
+
+server = NaServer(REPLACEME, 1, 21)
 # Only used for testing
 server.set_port(443)
-user = user_name
-pw = password
+user = REPLACEME
+pw = REPLACEMEM
 # Basic server details
 server.set_transport_type('HTTPS')
 server.set_style('LOGIN')
@@ -60,5 +62,9 @@ server.set_admin_user(user, pw)
 
 # Define a main function, more 'pythonicly'
 if __name__ == "__main__":
-    for i in get_all_obj():
-        print get_counters(i)    
+    the_app = GetAllObj()
+    for i in the_app.getList():
+        next_app = GetCounters(i)
+	print next_app.getOut()
+
+    
